3dd33b915b1815b654ad4ec0ccfa863265a47f16
Simon Brem <simon.brem@daimler.com>
2023-04-06 11:13:10 +0200

    layout adjustments

--- rd.js
+++ rd.js
@@ -1,6 +1,6 @@
 (function () {
     // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    var margin = { top: 10, right: 30, bottom: 30, left: 100 },
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
